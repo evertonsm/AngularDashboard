6175e1f33bb166551d769fdf516379262c28935d
Ensley <ensleyfmr@hotmail.com>
2018-10-02 20:32:40 -0300

    .....

--- controllers/userController.js
+++ controllers/userController.js
@@ -15,52 +15,22 @@ var url = "mongodb://localhost:27017/";
 
 router.post('/register', function(req, res) {
   
-    var typeUser = 'user';
-  
-  var hashedPassword = bcrypt.hashSync(req.body.password, 8);
-
-  User.create({
-    fullName: req.body.fullName,
-    email: req.body.email,
-    password: hashedPassword
-  },
-
+    var hashedPassword = bcrypt.hashSync(req.body.password, 8);
+    
+    User.create({
+      fullName : req.body.fullName,
+      email : req.body.email,
+      password : hashedPassword
+    },
     function (err, user) {
-
       if (err) return res.status(500).send("There was a problem registering the user.")
       // create a token
       var token = jwt.sign({ id: user._id }, config.secret, {
         //expiresIn: 86400 // expires in 24 hours
       });
-
       res.status(200).send({ auth: true, token: token });
-      console.log(user);
     }); 
-
-    
-      MongoClient.connect(url, function (err, db) {
-
-        console.log('Aqui 1');
-
-        if (err) throw err;
-
-        var dbo = db.db("dashboard");
-        
-        dbo.collection("userCollection").insertOne(user, function (err, result) {
-          if (err) throw err;
-          console.log('aqui = '+result)
-          
-          res.status(200).send({ auth: true, token: token });
-
-          db.close();
-
-        });
-
-
-      });
-
-    });
-
+  });
  
 
   router.get('/me', function (req, res) {
